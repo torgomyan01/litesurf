cc46525f28eca0cb947b6a7c4eaec32d9958dd64
Andranik Torgomyan <and.torgomyan01@gmail.com>
2024-10-13 12:54:54 +0400

    add mobil menu

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ const {
 
 const faqItem = document.querySelectorAll('.faq-item');
 const pricesSwitch = document.querySelector('.prices-switch');
+const mobileMenuBoard = document.querySelector('.mobile-menu-board');
+const mobileMenuBoardBody = document.querySelector('.mobile-menu-board-body');
+const mobileMenuBoardFon = document.querySelector('.mobile-menu-board-fon');
 
 faqItem.forEach((item) => {
   item.addEventListener('click', function (){
@@ -26,4 +29,16 @@ faqItem.forEach((item) => {
 
 pricesSwitch.addEventListener('click', function (){
   pricesSwitch.classList.toggle(active)
+})
+
+mobileMenuBoard.addEventListener('click', function (){
+  if(mobileMenuBoard.classList.contains(active)){
+    mobileMenuBoard.classList.remove(active);
+    mobileMenuBoardBody.classList.remove(active);
+    mobileMenuBoardFon.classList.remove(active);
+  } else {
+    mobileMenuBoard.classList.add(active);
+    mobileMenuBoardBody.classList.add(active);
+    mobileMenuBoardFon.classList.add(active);
+  }
 })
